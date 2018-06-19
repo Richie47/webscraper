3b8ec44ad0cf00ee559ac0f825965267d7296f30
Fortran001 <helixfossil001@gmail.com>
2018-06-18 22:46:23 -0700

    update

--- demonstration.py
+++ demonstration.py
@@ -1,8 +1,6 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-
-
 quote_page = 'https://www.espn.com/nba/team/roster/_/name/phx/phoenix-suns'
 
 print quote_page
@@ -11,10 +9,22 @@ page = urllib2.urlopen(quote_page)
 
 soup = BeautifulSoup(page, 'html.parser')
 
+#This makes an array of all the rows
+row_box = soup.find_all('tr', attrs={'class': 'oddrow'})
+
+#This makes an array of all elements in the first row
+units = row_box[0].find_all('td')
+
+
+#Examples
+print (units[7].text.strip())
+
+row = row_box[1].text.strip()
+
+print(row)
+
 name_box = soup.find('td', attrs={'class': 'sortcell'})
 
 name = name_box.text.strip()
 
 print(name)
-
-
