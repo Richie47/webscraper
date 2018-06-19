c637859c5861dc02df66bd8459f617e0d3dc36c8
Fortran001 <helixfossil001@gmail.com>
2018-06-18 21:49:17 -0700

    update

--- demonstration.py
+++ demonstration.py
@@ -1,19 +1,20 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-quote_page = 'http://www.bloomberg.com/quote/SPX:IND'
+
+
+quote_page = 'https://www.espn.com/nba/team/roster/_/name/phx/phoenix-suns'
+
+print quote_page
 
 page = urllib2.urlopen(quote_page)
 
 soup = BeautifulSoup(page, 'html.parser')
 
-name_box = soup.find('h1', attrs={'class': 'name'})
+name_box = soup.find('td', attrs={'class': 'sortcell'})
 
 name = name_box.text.strip()
+
 print(name)
 
-price_box = soup.find('div', attrs={'class':'price'})
 
-price = price_box.text
-
-print(price)
